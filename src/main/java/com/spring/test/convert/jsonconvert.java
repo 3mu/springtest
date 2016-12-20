d2e8e17634078948c3973e4a487be23d2b69d930
mulin <316428218@qq.com>
2016-12-20 23:19:00 +0800

    11

--- src/main/java/com/spring/test/convert/jsonconvert.java
+++ src/main/java/com/spring/test/convert/jsonconvert.java
@@ -1,10 +1,13 @@
 package com.spring.test.convert;
 
+import com.google.gson.GsonBuilder;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+
 /**
  * Created by Administrator on 2016/12/20 0020.
  */
-public class JsonConvert extends GsonHttpMessageConverter{
-    public MmoJsonConverter(){
+public class JsonConvert extends GsonHttpMessageConverter {
+    public JsonConvert(){
         //更换Gson转换器
         super.setGson(new GsonBuilder()
                 .serializeNulls()		//null值属性也需要序列化
