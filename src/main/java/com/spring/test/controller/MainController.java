6a9ac558ec187084bc923b4a465dcb558b247be5
mulin <316428218@qq.com>
2016-12-20 22:16:24 +0800

    tijiao

--- src/main/java/com/spring/test/controller/MainController.java
+++ src/main/java/com/spring/test/controller/MainController.java
@@ -1,30 +1,39 @@
 package com.spring.test.controller;
 
-import org.omg.CORBA.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
- * Created by Administrator on 2016/12/15 0015.
+ * Created by Administrator
+ * r on 2016/12/15 0015.
  */
 @Controller
 public class MainController {
-    public static final String INDEX = "Index";
-    public static final String API = "api";
+    private static final String INDEX;
+    private static final String API;
+
+    static {
+        API = "api";
+        INDEX = "Index";
+    }
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         
         return INDEX;
     }
 
+    @ResponseBody
     @RequestMapping(value = "/api",method = RequestMethod.GET)
-    public String hello(HttpServletRequest request, HttpServletResponse response){
-        request.setAttribute("name","this is a hello word");
-        return API;
+    public String hello(HttpServletRequest request){
+//        request.setAttribute("name","this is a hello word");
+//        return API;
+        return  "this is hello word";
     }
 }
+
+
